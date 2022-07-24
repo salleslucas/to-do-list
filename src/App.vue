a1239f7e0578e1c1b68cae760ec2f4463bc8c778
Lucas Salles <lucassalles@drrr.us>
2022-07-24 20:15:38 -0300

    feature: navbar + views de form e list

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Tarefa</b-nav-item>
+          <b-nav-item to="/form">Formulário</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
