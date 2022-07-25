69c64b4902e701ab6133c46a080d353b443b1eb3
Lucas Almeida Salles <lucas.almeida@g.globo>
2022-07-25 11:26:11 -0300

    Criação das views de formulário e da Lista

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="about">
-    <h1>Form</h1>
+  <div class="container mt-2">
+    <b-form>
+      <b-form-group 
+        label="Título"
+        label-for="subject"
+      >
+        <b-form-input
+          id="subject"
+          v-model="form.subject"
+          type="text"
+          placeholder="Ex: Lavar carro"
+          required
+          autocomplete="off"
+        >
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group 
+        label="Descrição"
+        label-for="description"
+      >
+        <b-form-textarea
+          id="description"
+          v-model="form.description"
+          type="text"
+          placeholder="Ex: Preciso levar o carro pra lavar"
+          required
+          autocomplete="off"
+        ></b-form-textarea>
+      </b-form-group>
+      <b-button type="submit" variant="outline-primary">
+        Salvar
+      </b-button>
+    </b-form>
+
   </div>
 </template>
+
+<script>
+export default {
+  name: "Form",
+
+  data() {
+    return {
+      form: {
+        subject: "",
+        description: "",
+      }
+    }
+  }
+}
+</script>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>List</h1>
+  <div class="container mt-2">
+    <b-card title="Titulo">
+      <b-card-text>descrição</b-card-text>
+      <b-button variant="outline-secondary" class="mr-2">Editar</b-button>
+      <b-button variant="outline-danger" class="mr-2">Excluir</b-button>
+    </b-card>
   </div>
 </template>
